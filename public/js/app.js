4d205cc32c428022524fec692c7905fc44398083
eanzalone <emily.anzalone@gmail.com>
2015-10-19 04:48:17 -0700

    Adding ajax (in progress)

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,33 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-	$('#postSubmit').on('click', function(e){
-		e.preventDefault();
-		// is click working?
-		console.log('Send clicked');
-		// push inputText to postSection
-		var blogText = $('#inputText').val();
-		console.log(blogText);
-		// if (blogText.trim.length>0){
-		// 	console.log('Content avail to post');
-		// }
-		// ACTUAL POST ADDED
-		$('#postSection').prepend('<p>' + blogText + '</p><hr>');
+	// $('#submitBtn').on('click', function(e){
+	// 	e.preventDefault();
+	// 	// is click working?
+	// 	console.log('Send clicked');
+	// 	// push inputText to postSection
+	// 	var blogText = $('#inputText').val();
+	// 	console.log(blogText);
+	// 	// ignore empty posts
+	// 	// if (blogText.trim().length>0){
+	// 	// 	console.log('Content avail to post:', blogText.length);
+	// 	// }
+	// 	// ACTUAL POST ADDED
+	// 	$('#postSection').prepend('<p>' + blogText + '</p><hr>');
+	// });
+
+
+$('#newBlogPost').on('submit', function(e){
+	e.preventDefault();
+	var data = $(this);
+	console.log(data);
+	$.post('/api/posts', data, function(newPost){
+		console.log(newPost);
+		$('#postSection').prepend('<p>' + blogText + '<span class="close" data-id='+newPost.id+'>x</span></p>');
 	});
+	$('#newBlogPost').children().val('');
+});
+
+
 
 });
--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 // REQUIREMENTS
-var express = require('express');
-var app = express();
+var express = require("express"),
+    app = express(),
+    path = require("path"),
+    bodyParser = require("body-parser");
 
 // CONFIG
 // set ejs as view engine
 app.set('view engine', 'ejs');
 // serve js & css files
 app.use("/static", express.static("public"));
+// body parser config to accept our datatypes
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // DATA
+
+var posts = [
+	{id: 0, text: "Hardcoded Object"},
+	{id: 1, text: "Item 2"}
+];
+
 app.get('/', function (req, res) {
   //res.send('Hello World!');
   res.render('index');
+});
+
+// Is the data connected?
+app.get('/api/posts', function (req, res){
+	// send posts data as JSON
+	res.json(posts);
+});
+
+app.post('/api/posts', function(req, res){
+	var newFood = req.body;
+	console.log(newFood);
 });
 
 //LISTENING TO A PORT
